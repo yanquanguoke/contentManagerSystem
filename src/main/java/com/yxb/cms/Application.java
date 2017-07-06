d759e9a0789af610cd027d50cc42e1719d2456f8
yangxiaobing <873559947@qq.com>
2017-07-06 14:16:03 +0800

    springBoot 配置初始化

---
+++ src/main/java/com/yxb/cms/Application.java
@@ -0,0 +1,28 @@
+package com.yxb.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Spring boot 主类入口测试
+ *
+ * @author yangxiaobing
+ * @date 2017年4月5日
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer{
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+
+	
+    public static void main(String[] args) throws Exception {
+    	SpringApplication.run(Application.class, args);  
+    }
+}
+
