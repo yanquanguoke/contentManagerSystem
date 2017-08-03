5e8e56c053f8d2d77153cad04ac1663dec7af2e2
yangxiaobing <873559947@qq.com>
2017-08-03 20:01:03 +0800

    版本更新

--- src/main/java/com/yxb/cms/Application.java
+++ src/main/java/com/yxb/cms/Application.java
@@ -48,6 +48,7 @@ public class Application extends SpringBootServletInitializer{
 	
 	@Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        setRegisterErrorPageFilter(false);
         return application.sources(Application.class);
     }
 
