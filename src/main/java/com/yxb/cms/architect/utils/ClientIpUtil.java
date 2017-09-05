3b72f950d7d1a0abdb7de0ac9830b4607098b8f8
YXB <873559947@qq.com>
2017-09-05 23:54:33 +0800

    页面UI优化

---
+++ src/main/java/com/yxb/cms/architect/utils/ClientIpUtil.java
@@ -0,0 +1,97 @@
+/**
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2017 © yangxiaobing, 873559947@qq.com
+ *
+ * This file is part of contentManagerSystem.
+ * contentManagerSystem is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * contentManagerSystem is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with contentManagerSystem.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * 这个文件是contentManagerSystem的一部分。
+ * 您可以单独使用或分发这个文件，但请不要移除这个头部声明信息.
+ * contentManagerSystem是一个自由软件，您可以自由分发、修改其中的源代码或者重新发布它，
+ * 新的任何修改后的重新发布版必须同样在遵守GPL3或更后续的版本协议下发布.
+ * 关于GPL协议的细则请参考COPYING文件，
+ * 您可以在contentManagerSystem的相关目录中获得GPL协议的副本，
+ * 如果没有找到，请连接到 http://www.gnu.org/licenses/ 查看。
+ *
+ * - Author: yangxiaobing
+ * - Contact: 873559947@qq.com
+ * - License: GNU Lesser General Public License (GPL)
+ * - source code availability: http://git.oschina.net/yangxiaobing_175/contentManagerSystem
+ */
+package com.yxb.cms.architect.utils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @author yangxiaobing
+ * @date 2017/9/5 17:18
+ */
+
+
+public class ClientIpUtil {
+
+
+    public static String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+            if (ip.equals("127.0.0.1")) {
+                //根据网卡取本机配置的IP
+                InetAddress inet = null;
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                ip = inet.getHostAddress();
+            }
+        }
+        // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+        if (ip != null && ip.length() > 15) {
+            if (ip.indexOf(",") > 0) {
+                ip = ip.substring(0, ip.indexOf(","));
+            }
+        }
+        return ip;
+    }
+
+
+    public static String decodeUnicode(final String dataStr) {
+        int start = 0;
+        int end = 0;
+        final StringBuffer buffer = new StringBuffer();
+        while (start > -1) {
+            end = dataStr.indexOf("\\u", start + 2);
+            String charStr = "";
+            if (end == -1) {
+                charStr = dataStr.substring(start + 2, dataStr.length());
+            } else {
+                charStr = dataStr.substring(start + 2, end);
+            }
+            char letter = (char) Integer.parseInt(charStr, 16); // 16进制parse整形字符串。
+            buffer.append(new Character(letter).toString());
+            start = end;
+        }
+        return buffer.toString();
+    }
+}
