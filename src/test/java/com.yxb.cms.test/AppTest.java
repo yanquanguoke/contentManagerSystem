9de445bcbeba74d4ae4e80a9df1c0bc625f39563
yangxiaobing <873559947@qq.com>
2018-04-27 18:19:16 +0800

    spring redis

--- src/test/java/com.yxb.cms.test/AppTest.java
+++ src/test/java/com.yxb.cms.test/AppTest.java
@@ -1,23 +1,18 @@
 package com.yxb.cms.test;
 
 import com.yxb.cms.Application;
-import com.yxb.cms.architect.properties.JdbcProperties;
-import com.yxb.cms.architect.utils.HttpUtil;
-import com.yxb.cms.dao.SystemLogMapper;
-import com.yxb.cms.domain.dto.DataCollectDto;
-import com.yxb.cms.service.DataCleaningService;
-import com.yxb.cms.service.ResourceService;
-import com.yxb.cms.service.UserService;
-import net.sf.json.JSONObject;
+import com.yxb.cms.service.RedisService;
+import org.apache.commons.collections.map.HashedMap;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.nutz.json.Json;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -31,25 +26,21 @@ import java.util.Map;
 public class AppTest {
 
     @Autowired
-    private JdbcProperties jdbcProperties;
+    RedisService redisService;
 
-    @Autowired
-    private UserService userService;
 
-    @Autowired
-    private SystemLogMapper systemLogMapper;
 
-    @Autowired
-    DataCleaningService dataCleaningService;
+
 
     @Test
     public void test() {}
 
     @Test
     public void test2() throws Exception{
-       Map<String,Object> map =  dataCleaningService.selectEchartsByLoginInfo();
-        System.out.println(Json.toJson(map));
+        System.out.println();
 
+
+        System.out.println("---"+redisService.ping());
     }
 
 
